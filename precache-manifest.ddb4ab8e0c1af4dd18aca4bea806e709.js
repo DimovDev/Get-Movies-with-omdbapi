adddb93b0d572ecd2ff57f6745e7533cfb674012
Ivan Dimov <iv_dimov@abv.bg>
2020-07-05 13:13:37 +0300

    Updates

---
+++ precache-manifest.ddb4ab8e0c1af4dd18aca4bea806e709.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a252d63b77c77ef51bf7f9829c3afe96",
+    "url": "/Get-Movies-with-omdbapi/index.html"
+  },
+  {
+    "revision": "249e25fdb5d9f57ebf34",
+    "url": "/Get-Movies-with-omdbapi/static/css/main.361dcff1.chunk.css"
+  },
+  {
+    "revision": "04d44b44f8d0661c7f6f",
+    "url": "/Get-Movies-with-omdbapi/static/js/2.69bb2aac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Get-Movies-with-omdbapi/static/js/2.69bb2aac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "249e25fdb5d9f57ebf34",
+    "url": "/Get-Movies-with-omdbapi/static/js/main.49ed81b1.chunk.js"
+  },
+  {
+    "revision": "fab660c6f2e3515b2d30",
+    "url": "/Get-Movies-with-omdbapi/static/js/runtime-main.cc651472.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.87f82044d7ebbd5fd75aed5273eb5108.js"
+  "/Get-Movies-with-omdbapi/precache-manifest.ddb4ab8e0c1af4dd18aca4bea806e709.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Get-Movies-with-omdbapi/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
