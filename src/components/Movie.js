d295ea96e07fc3ef9f2590d603c26ec673d42658
Ivan Dimov <iv_dimov@abv.bg>
2020-07-05 12:25:04 +0300

    add git hub pages

---
+++ src/components/Movie.js
@@ -0,0 +1,21 @@
+import React from "react";
+const DEFAULT_PLACEHOLDER_IMAGE =
+    "https://images.unsplash.com/photo-1534068590799-09895a701e3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80";
+const Movie = ({ movie }) => {
+    const poster =
+        movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
+    return (
+        <div className="movie">
+            <h2>{movie.Title}</h2>
+            <div>
+                <img
+                    width="200"
+                    alt={`The movie titled: ${movie.Title}`}
+                    src={poster}
+                />
+            </div>
+            <p>({movie.Year})</p>
+        </div>
+    );
+};
+export default Movie;
